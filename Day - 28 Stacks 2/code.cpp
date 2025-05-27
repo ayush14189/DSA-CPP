31c6d09d744ba68bf5a070fd5c5d7c00a4c34072
Ayush Jain <ayushjain14189@gmail.com>
2025-05-27 22:13:43 +0530

    Stacks updated

--- Day - 28 Stacks 2/code.cpp
+++ Day - 28 Stacks 2/code.cpp
@@ -74,7 +74,69 @@ bool isDuplicate(string str) {
     return false;
 }
 
+void printArr(vector<int> arr){
+    for(int i=0; i<arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 // Max Area in Histogram
+void maxAreaHistogram(vector<int> height) {
+    int n = height.size();
+    vector<int> nsl(n);
+    vector<int> nsr(n);
+    stack<int> s;
+
+    // next smaller left
+    nsl[0] = -1;
+    s.push(0);
+    for(int i=1; i<height.size(); i++) {
+        int curr = height[i];
+        while(!s.empty() && curr <= height[s.top()]) {
+            s.pop();
+        }
+        if(s.empty()){
+            nsl[i] = -1;
+        } else {
+            nsl[i] = s.top();
+        }
+        s.push(i);
+    }
+
+    printArr(nsl);
+
+    while(!s.empty()) {
+        s.pop();
+    }
+    
+    // next smaller right
+    nsr[n-1] = n;
+    s.push(n-1);
+    for(int i=n-2; i>=0; i--) {
+        int curr = height[i];
+        while(!s.empty() && curr <= height[s.top()]) {
+            s.pop();
+        }
+        if(s.empty()){
+            nsr[i] = n;
+        } else {
+            nsr[i] = s.top();
+        }
+        s.push(i);
+    }
+
+    printArr(nsr);
+
+    int maxArea = 0;
+    for(int i=0; i<n; i++) {
+        int ht = height[i];
+        int width = nsr[i] - nsl[i] - 1;
+        int area = ht*width;
+        maxArea = max(area, maxArea);
+    }
+    cout << "Maximum Area = " <<  maxArea;
+}
 
 int main() {
 // Next Greater Element
@@ -99,6 +161,8 @@ int main() {
     cout << isDuplicate(s2) << endl; // 0
 
 // Max Area in Histogram
+    vector<int> height = {2, 1, 5, 6, 2, 3};
+    maxAreaHistogram(height);
 
     return 0;
 }
