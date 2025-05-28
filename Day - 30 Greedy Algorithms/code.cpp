dd0a943944c584bf107fdfef57d9bc6c44314b8f
Ayush Jain <ayushjain14189@gmail.com>
2025-05-28 14:33:10 +0530

    Files added

---
+++ Day - 30 Greedy Algorithms/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Activity Selection Problem
+    // Pairs in C++
+    // Fractional Knsapsack
+    // Min Absolute Difference of Pairs
+    // Max Chain Length
+    // Indian Coins
+    // Job Sequencing Problem
+
+    return 0;
+}
