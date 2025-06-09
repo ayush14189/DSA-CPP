2a239b1782e52c68418836e473817eaec5ddd0f1
Ayush Jain <ayushjain14189@gmail.com>
2025-06-10 02:16:18 +0530

    Graphs updated

--- Day - 45 Graphs 5/code.cpp
+++ Day - 45 Graphs 5/code.cpp
@@ -5,7 +5,35 @@ using namespace std;
 
 
 // Disjoint Set
+class DisjointSet{
+public:
+    int n;
+    vector<int> par;
+    vector<int> rank;
 
+    DisjointSet(int n) {
+        this->n = n;
+        for(int i=0; i<n; i++) {
+            par[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    int find(int x) {
+        if(par[x] == x) {
+            return x;
+        }
+        return find(par[x]);
+    }
+    int unionByRank(int a, int b) {
+        int parA = find(a);
+        int parB = find(b);
+
+        if(rank[parA] == rank[parB]) {
+            
+        }
+    }
+};
 
 // Kruskal's Algorithm
 
