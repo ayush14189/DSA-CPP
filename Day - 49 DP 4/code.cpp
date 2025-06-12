856357c120f1a2ae347aae415d720ea740e75c8e
Ayush Jain <ayushjain14189@gmail.com>
2025-06-12 16:36:37 +0530

    DP updated

--- Day - 49 DP 4/code.cpp
+++ Day - 49 DP 4/code.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <string>
 #include <unordered_set>
 using namespace std;
@@ -43,7 +44,7 @@ int LIS(vector<int> arr) {
     int m = arr2.size();
     vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
     
-    for(int i=1, i<n+1; i++) { // O(n*n)
+    for(int i=1; i<n+1; i++) { // O(n*n)
         for(int j=1; j<m+1; j++) {
             if(arr[i-1] == arr2[j-1]) {
                 dp[i][j] = 1 + dp[i-1][j-1];
@@ -55,8 +56,32 @@ int LIS(vector<int> arr) {
     return dp[n][m];
 }
 
-// Edit Distance
+// Edit Distance - LeetCode 72
+int minDistance(string word1, string word2) { // O(n*m)
+    int n = word1.size();
+    int m = word2.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
+    for(int j=0; j<m+1; j++) { // 0th row initialize - insert
+        dp[0][j] = j;
+    }
+
+    for(int i=0; i<n+1; i++) { // 0th col initialize - delete
+        dp[i][0] = i;
+    }
+
+    // Bottom-Up Approach
+    for(int i=1; i<n+1; i++) {
+        for(int j=1; j<m+1; j++) {
+            if(word1[i-1] == word2[j-1]) {
+                dp[i][j] = 0 + dp[i-1][j-1];
+            } else {
+                dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+    }
+    return dp[n][m];
+}
 
 int main() {
     // Longest Common Substring
@@ -73,6 +98,11 @@ int main() {
     cout << "--------------------------------------------------------" << endl;
 
     // Edit Distance
+    string s1 = "horse";
+    string s2 = "ros";
+    cout << "Min Distance = " << minDistance(s1, s2) << endl;
+
+    cout << "--------------------------------------------------------" << endl;
 
     return 0;
 }
