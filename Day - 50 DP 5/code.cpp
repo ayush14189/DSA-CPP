c06e174aa52f6f8ac72ba1ab7f60a467d29fd48f
Ayush Jain <ayushjain14189@gmail.com>
2025-06-13 07:30:42 +0530

    DP updated

--- Day - 50 DP 5/code.cpp
+++ Day - 50 DP 5/code.cpp
@@ -2,21 +2,56 @@
 using namespace std;
 
 // Wildcard Matching
+
+
 // Catalan Number
+
+
 // Nth catalan (Recursion)
+
+
 // Nth catalan (Memoization)
+
+
 // Nth catalan (Tabulation)
+
+
 // Count BSTs from Nodes
+
+
 // Mountain Ranges
 
+
 int main() {
+    
     // Wildcard Matching
+    
+    
+    cout << "--------------------------------------------------------" << endl;
+    
     // Catalan Number
+    
+    cout << "--------------------------------------------------------" << endl;
+    
     // Nth catalan (Recursion)
+    
+    cout << "--------------------------------------------------------" << endl;
+    
     // Nth catalan (Memoization)
+    
+    cout << "--------------------------------------------------------" << endl;
+    
     // Nth catalan (Tabulation)
+    
+    cout << "--------------------------------------------------------" << endl;
+    
     // Count BSTs from Nodes
+
+    cout << "--------------------------------------------------------" << endl;
+
     // Mountain Ranges
+
+    cout << "--------------------------------------------------------" << endl;
 
     return 0;
 }
---
+++ Day - 52 Segment Trees/code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Creating Segment Tree
+// Queries on Segment Tree
+// Update on Segment Tree
+// Range Max Segment Tree (Creation)
+// Range Max Segment Tree (Query)
+// Range Max Segment Tree (Update)
+// Range Min Segment Tree
+
+int main() {
+    
+    // Creating Segment Tree
+    // Queries on Segment Tree
+    // Update on Segment Tree
+    // Range Max Segment Tree (Creation)
+    // Range Max Segment Tree (Query)
+    // Range Max Segment Tree (Update)
+    // Range Min Segment Tree
+
+    return 0;
+}
